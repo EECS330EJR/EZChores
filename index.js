5ee7def5aca99546caad2fcdc9ac37f15e099526
John Nguyen <johntnguyen9@gmail.com>
2019-11-13 11:07:35 -0600

    Add modal js

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-const button = document.querySelector(".foo-button");
-mdc.ripple.MDCRipple.attachTo(button);
+// const button = document.querySelector(".foo-button");
+// mdc.ripple.MDCRipple.attachTo(button);
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  console.log('ahahaha')
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
